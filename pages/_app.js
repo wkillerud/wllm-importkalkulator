810cc61725661acdd65de9b6aba3a2e0e7248738
William Killerud <william@killerud.com>
2023-02-23 21:01:20 +0100

    chore: update dependencies

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
 import "../styles/globals.scss";
 import "@fremtind/jkl-core/core.scss";
+import "@fremtind/jkl-input-group/input-group.scss";
 import "@fremtind/jkl-select/select.scss";
-import "@fremtind/jkl-text-input/text-input.scss";
 import "@fremtind/jkl-summary-table/summary-table.scss";
+import "@fremtind/jkl-text-input/text-input.scss";
 
 function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
